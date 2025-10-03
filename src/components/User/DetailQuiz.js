538741cdcb42bf77b4ea211ddb75affac1e84287
project 01 <21522528@gm.uit.edu.vn>
2025-10-03 14:47:08 +0700

    Refactor DetailQuiz component to include breadcrumb navigation and improve layout; adjust styles for better spacing and responsiveness

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -6,6 +6,8 @@ import "./DetailQuiz.scss";
 import Question from "./Question";
 import ModalResult from "./ModalResult";
 import RightContent from "./Content/RightContent";
+import Breadcrumb from "react-bootstrap/Breadcrumb";
+import { NavLink } from "react-router-dom";
 
 const DetailQuiz = () => {
     const params = useParams();
@@ -43,7 +45,7 @@ const DetailQuiz = () => {
                         item.answers.issSelected = false;
                         answers.push(item.answers);
                     });
-                    answers = _.orderBy(answers,['id'],['asc'])
+                    answers = _.orderBy(answers, ["id"], ["asc"]);
                     return {
                         questionId: key,
                         answers,
@@ -128,62 +130,76 @@ const DetailQuiz = () => {
     };
 
     return (
-        <div className="detail-quiz-container">
-            <div className="left-content">
-                <div className="title">
-                    Quiz: {quizId} : {location?.state?.quizTitle}
+        <>
+            <Breadcrumb className="breadcrumb">
+                <NavLink to="/" className="breadcrumb-item">
+                    Home
+                </NavLink>
+
+                <NavLink to="/users" className="breadcrumb-item">
+                    Users
+                </NavLink>
+
+                <Breadcrumb.Item active>Quiz {quizId}</Breadcrumb.Item>
+            </Breadcrumb>
+
+            <div className="detail-quiz-container">
+                <div className="left-content">
+                    <div className="title">
+                        Quiz: {quizId} : {location?.state?.quizTitle}
+                    </div>
+                    <hr></hr>
+                    <div className="q-content">
+                        <Question
+                            index={index}
+                            handleCheckbox={handleCheckbox}
+                            data={
+                                dataQuiz && dataQuiz.length > 0
+                                    ? dataQuiz[index]
+                                    : []
+                            }
+                        />
+                    </div>
+                    <div className="footer">
+                        <button
+                            className="btn btn-secondary"
+                            onClick={() => {
+                                handlePrev();
+                            }}
+                        >
+                            Prev
+                        </button>
+                        <button
+                            className="btn btn-primary"
+                            onClick={() => {
+                                handleNext();
+                            }}
+                        >
+                            Next
+                        </button>
+                        <button
+                            className="btn btn-warning"
+                            onClick={() => handleFinishQuiz()}
+                        >
+                            Finish
+                        </button>
+                    </div>
                 </div>
-                <hr></hr>
-                <div className="q-content">
-                    <Question
+                <div className="right-content">
+                    <RightContent
+                        dataQuiz={dataQuiz}
+                        handleFinishQuiz={handleFinishQuiz}
+                        setIndex={setIndex}
                         index={index}
-                        handleCheckbox={handleCheckbox}
-                        data={
-                            dataQuiz && dataQuiz.length > 0
-                                ? dataQuiz[index]
-                                : []
-                        }
-                    />
+                    ></RightContent>
                 </div>
-                <div className="footer">
-                    <button
-                        className="btn btn-secondary"
-                        onClick={() => {
-                            handlePrev();
-                        }}
-                    >
-                        Prev
-                    </button>
-                    <button
-                        className="btn btn-primary"
-                        onClick={() => {
-                            handleNext();
-                        }}
-                    >
-                        Next
-                    </button>
-                    <button
-                        className="btn btn-warning"
-                        onClick={() => handleFinishQuiz()}
-                    >
-                        Finish
-                    </button>
-                </div>
+                <ModalResult
+                    show={isShowModalResult}
+                    setShow={setIsShowModalResult}
+                    dataModalResult={dataModalResult}
+                ></ModalResult>
             </div>
-            <div className="right-content">
-                <RightContent
-                    dataQuiz={dataQuiz}
-                    handleFinishQuiz={handleFinishQuiz}
-                    setIndex={setIndex}
-                    index={index}
-                ></RightContent>
-            </div>
-            <ModalResult
-                show={isShowModalResult}
-                setShow={setIsShowModalResult}
-                dataModalResult={dataModalResult}
-            ></ModalResult>
-        </div>
+        </>
     );
 };
 export default DetailQuiz;
